e402622a89138defbecdbfb5bf5d920222a3c8a8
Yog <yog.panjarale@gmail.com>
2021-08-03 23:40:08 +0530

    part one done

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+mod utils;
+
 ///Number
 #[derive(Debug, PartialEq)]
 pub struct Number(pub i32);
 
 impl Number {
-    pub fn new(s: &str) -> Self {
-        return Self(s.parse().unwrap());
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, number) = utils::extract_digits(s);
+        (s, Self(number.parse().unwrap()))
     }
 }
 
@@ -17,40 +20,77 @@ pub enum Op {
     Div,
 }
 impl Op {
-    pub fn new(s: &str) -> Self {
-        match s {
-            "+" =>Self::Add,
-            "-" =>Self::Sub,
-            "*" =>Self::Mul,
-            "/" =>Self::Div,
-            _ => panic!("Bad Operator")
-        }
+    pub fn new(s: &str) -> (&str, Self)  {
+        let (s, op) = utils::extract_op(s);
+
+        let op = match op {
+            "+" => Self::Add,
+            "-" => Self::Sub,
+            "*" => Self::Mul,
+            "/" => Self::Div,
+            _ => unreachable!(),
+        };
+
+        (s, op)
     }
 }
 
+///Expression
+#[derive(Debug, PartialEq)]
+pub struct Expr {
+    pub lhs: Number,
+    pub rhs: Number,
+    pub op: Op,
+}
+impl Expr {
+    pub fn new(s: &str) -> (&str, Self) {
+        let (s, lhs) = Number::new(s);
+        let (s, op) = Op::new(s);
+        let (s, rhs) = Number::new(s);
+
+        (s, Self { lhs, rhs, op })
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn parse_number() {
-      assert_eq!(Number::new("123"), Number(123));
+        assert_eq!(Number::new("123"), ("", Number(123)));
     }
+
     #[test]
     fn parse_add_op() {
-        assert_eq!(Op::new("+"),Op::Add);
+        assert_eq!(Op::new("+"), ("", Op::Add));
     }
+
     #[test]
-    fn parse_sun_op() {
-        assert_eq!(Op::new("-"),Op::Sub);
+    fn parse_sub_op() {
+        assert_eq!(Op::new("-"), ("", Op::Sub));
     }
+
     #[test]
     fn parse_mul_op() {
-        assert_eq!(Op::new("*"),Op::Mul);
+        assert_eq!(Op::new("*"), ("", Op::Mul));
     }
+
     #[test]
     fn parse_div_op() {
-        assert_eq!(Op::new("/"),Op::Div);
+        assert_eq!(Op::new("/"), ("", Op::Div));
     }
-    
+    #[test]
+    fn parse_one_plus_two() {
+        assert_eq!(
+            Expr::new("1+2"),
+            (
+                "",
+                Expr {
+                    lhs: Number(1),
+                    rhs: Number(2),
+                    op: Op::Add,
+                },
+            ),
+        );
+    }
 }
