dfeaebe354c3332aa0180ee851714e74415b3fee
Yog <yog.panjarale@gmail.com>
2021-08-03 22:53:50 +0530

    number and operator done

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,56 @@
+///Number
+#[derive(Debug, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> Self {
+        return Self(s.parse().unwrap());
+    }
+}
+
+///Operator
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+impl Op {
+    pub fn new(s: &str) -> Self {
+        match s {
+            "+" =>Self::Add,
+            "-" =>Self::Sub,
+            "*" =>Self::Mul,
+            "/" =>Self::Div,
+            _ => panic!("Bad Operator")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn parse_number() {
+      assert_eq!(Number::new("123"), Number(123));
     }
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("+"),Op::Add);
+    }
+    #[test]
+    fn parse_sun_op() {
+        assert_eq!(Op::new("-"),Op::Sub);
+    }
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("*"),Op::Mul);
+    }
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("/"),Op::Div);
+    }
+    
 }
